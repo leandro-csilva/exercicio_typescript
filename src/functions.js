b70fc1b3bee8a9f977eb1ab74771e4c8e578e437
Leandro Silva <leandro.csilva356@gmail.com>
2024-06-06 22:17:16 -0300

    commit inicial

---
+++ src/functions.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplicar(numero1 = 5, numero2 = 10) {
+    return numero1 * numero2;
+}
+console.log(multiplicar());
+function concatenar(nome = "Leandro") {
+    return "Olá " + nome;
+}
+console.log(concatenar());
---
+++ src/functions.ts
@@ -0,0 +1,11 @@
+function multiplicar(numero1: number = 5, numero2: number = 10): number {
+    return numero1 * numero2;
+}
+
+console.log(multiplicar());
+
+function concatenar(nome: string = "Leandro"): string {
+    return "Olá " + nome;
+}
+
+console.log(concatenar());
